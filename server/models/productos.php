58daf7de1ce27512cd9c1d631018dd13312a3c8b
TomyVarasOviedo <tomasvarasoviedo@gmail.com>
2021-12-03 22:33:51 -0300

    Mostrar Productos: Ajuste

--- server/models/productos.php
+++ server/models/productos.php
@@ -2,6 +2,6 @@
 function mostrar_productos() {
     include_once("./conexion.php");
     $consulta = $conexion->query("SELECT * FROM `productos`INNER JOIN categorias ON productos.fk_categoria = categorias.id_categoria");
-    $row = $consulta->fetch_all();
+    $row = $consulta->fetch_all(1);
     return json_encode($row);
 }
