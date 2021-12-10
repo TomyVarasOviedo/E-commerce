7b0cdc3ffd22a41233804ebae40eeca9f139e041
TomyVarasOviedo <tomasvarasoviedo@gmail.com>
2021-12-09 21:43:54 -0300

    Backend: Carrito: Agregar

--- server/routes/carrito.php
+++ server/routes/carrito.php
@@ -3,3 +3,11 @@ router("GET", "/carrito/:user", function ($user){
    include_once("./models/carrito.php");
    echo mostrar_carrito($user);
 });
+router("POST", "/carrito-agregar",function (){
+    include_once("./models/carrito.php");
+    $datos = new stdClass();
+    $datos->id_usuario = $_POST['usuario'];
+    $datos->id_producto = $_POST['producto'];
+
+    echo agregar_carrito($datos);
+});
